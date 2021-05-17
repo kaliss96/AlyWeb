8e9dfadab950dcd58423ebd45cab579d821cdb64
kaliss96 <aguirretellez96@gmail.com>
2021-05-17 15:17:44 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,26 +7,28 @@ import Login from "./components/Auth/Login";
 // import router config
 import { BrowserRouter} from "react-router-dom"
 import GuestRoute from './components/Commons/GuestRoute'
-import {RDX_LOADUSER} from './redux/actions/auth'
+import PrivateRoute from './components/Commons/PrivateRoute'
+import { loadUser } from './redux/actions/auth'
 import Dashboard from "./components/Dashboard/dashboard.view"
 import Register from "./components/Auth/Register.jsx"
 
-function App() {
-  useEffect(function () {
-    store.dispatch(RDX_LOADUSER)
+const App = ({location}) => {
+
+  useEffect(()=> {
+    store.dispatch(loadUser())
   }, [])
 
   return (
     <div className="App">
- 
+
       <BrowserRouter>
           <Provider store={store}>
-            <GuestRoute exact path = "/dashboard" component={Dashboard}/>
-            <GuestRoute exact path = "/" component={Login}/> 
-            <GuestRoute exact path = "/register" component={Register}/>
+            <PrivateRoute location={location} exact path = "/dashboard" component={Dashboard}/>
+            <GuestRoute location={location} exact path = "/" component={Login}/> 
+            <GuestRoute location={location} exact path = "/register" component={Register}/>
           </Provider>
       </BrowserRouter>
- 
+
     </div>
   );
 }
